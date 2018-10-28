ba353a50356370537a349a725d09b1efa936c29f
shrbenic1 <shrbenic1@etf.unsa.ba>
2018-10-28 17:32:45 +0100

    Dodani atributi klase FiksniBroj i napisan njen konstruktor

--- src/ba/unsa/etf/rpr/tutorijal03/FiksniBroj.java
+++ src/ba/unsa/etf/rpr/tutorijal03/FiksniBroj.java
@@ -1,8 +1,13 @@
 package ba.unsa.etf.rpr.tutorijal03;
 
 public class FiksniBroj implements TelefonskiBroj{
+    String broj;
+    Grad grad;
     public String ispisi() {
         return "";
     }
-
+    public FiksniBroj(Grad grad, String broj) {
+        this.grad=grad;
+        this.broj=broj;
+    }
 }
