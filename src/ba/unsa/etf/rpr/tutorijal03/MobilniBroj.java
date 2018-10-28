d4514f58ec604d9c73c665f05d71d70b7e7f5a54
shrbenic1 <shrbenic1@etf.unsa.ba>
2018-10-28 17:40:56 +0100

    Kreirana klasa MobilniBroj zajedno s konstruktorom

---
+++ src/ba/unsa/etf/rpr/tutorijal03/MobilniBroj.java
@@ -0,0 +1,10 @@
+package ba.unsa.etf.rpr.tutorijal03;
+
+public class MobilniBroj {
+    private int mobilnaMreza;
+    String broj;
+    public MobilniBroj(int mobilnaMreza, String broj) {
+        this.mobilnaMreza=mobilnaMreza;
+        this.broj=broj;
+    }
+}
