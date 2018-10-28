94b56c0613a212c4f13bd7b09490340a3b17dca9
shrbenic1 <shrbenic1@etf.unsa.ba>
2018-10-28 18:08:13 +0100

    Kreirana klasa Imenik s metodom "dodaj"

--- src/ba/unsa/etf/rpr/tutorijal03/Imenik.java
+++ src/ba/unsa/etf/rpr/tutorijal03/Imenik.java
@@ -1,4 +1,10 @@
 package ba.unsa.etf.rpr.tutorijal03;
 
+import java.util.HashMap;
+
 public class Imenik {
+    private HashMap imenik;
+    void dodaj(String ime, TelefonskiBroj broj) {
+        imenik.put(broj, ime);
+    }
 }
