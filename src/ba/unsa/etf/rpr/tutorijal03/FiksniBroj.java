2ba56b416e36ad6ac27e404937cbd2b4e24ff041
shrbenic1 <shrbenic1@etf.unsa.ba>
2018-10-29 23:54:44 +0100

    Uvedena custom numeracija enuma

--- src/ba/unsa/etf/rpr/tutorijal03/FiksniBroj.java
+++ src/ba/unsa/etf/rpr/tutorijal03/FiksniBroj.java
@@ -1,21 +1,36 @@
 package ba.unsa.etf.rpr.tutorijal03;
 
 public class FiksniBroj implements TelefonskiBroj{
-    public enum Grad { TRAVNIK, ORASJE, ZENICA, SARAJEVO, LIVNO, TUZLA, MOSTAR, BIHAC, GORAZDE, SIROKIBRIJEG, BRCKO, MRKONJICGRAD, BANJALUKA, PRIJEDOR, DOBOJ, SAMAC, BIJELJINA, ZVORNIK, PALE, FOCA, TREBINJE}
+    public enum Grad {
+        TRAVNIK (30), ORASJE (31), ZENICA (32), SARAJEVO (33), LIVNO (34), TUZLA (35), MOSTAR (36), BIHAC (37), GORAZDE (38), SIROKIBRIJEG (39), BRCKO (49), MRKONJICGRAD (50), BANJALUKA (51), PRIJEDOR (52), DOBOJ (53), SAMAC (54), BIJELJINA (55), ZVORNIK (56), PALE (57), FOCA (58), TREBINJE (59);
+
+        private final int broj;
+
+        Grad(int broj) {
+            this.broj=broj;
+        }
+        public int getBroj() {
+            return broj;
+        }
+    }
+
     private String broj;
     private Grad grad;
 
     public String ispisi() {
-        return grad + "/" + broj;
+        return "0" +  grad.getBroj() + "/" + broj;
     }
+
     public FiksniBroj(Grad grad, String broj) {
         this.grad=grad;
         this.broj=broj;
     }
+
     @Override
     public int hashCode() {
         return grad.hashCode();
     }
+
     @Override
     public boolean equals (Object o) {
         if (o == null) {
