177525bc5eb494ada7e2b7350a570d6dc4a019ec
shrbenic1 <shrbenic1@etf.unsa.ba>
2018-10-29 23:57:53 +0100

    Uređen izgled koda klase MobilniBroj

--- src/ba/unsa/etf/rpr/tutorijal03/MobilniBroj.java
+++ src/ba/unsa/etf/rpr/tutorijal03/MobilniBroj.java
@@ -3,17 +3,21 @@ package ba.unsa.etf.rpr.tutorijal03;
 public class MobilniBroj implements TelefonskiBroj {
     private int mobilnaMreza;
     private String broj;
+
     public MobilniBroj(int mobilnaMreza, String broj) {
         this.mobilnaMreza=mobilnaMreza;
         this.broj=broj;
     }
+
     public String ispisi() {
         return "0" + mobilnaMreza + "/" + broj;
     }
+
     @Override
     public int hashCode() {
         return mobilnaMreza;
     }
+
     @Override
     public boolean equals (Object o) {
         if (o == null) {
