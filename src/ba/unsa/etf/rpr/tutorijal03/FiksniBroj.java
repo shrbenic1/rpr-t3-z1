952c479f4fcf4acdbfa5c55d067b4b33068f6e48
shrbenic1 <shrbenic1@etf.unsa.ba>
2018-10-29 19:46:19 +0100

    Preklopljeni operatori equals i hashcode za FiksniBroj

--- src/ba/unsa/etf/rpr/tutorijal03/FiksniBroj.java
+++ src/ba/unsa/etf/rpr/tutorijal03/FiksniBroj.java
@@ -3,11 +3,27 @@ package ba.unsa.etf.rpr.tutorijal03;
 public class FiksniBroj implements TelefonskiBroj{
     private String broj;
     private Grad grad;
+
     public String ispisi() {
         return grad + "/" + broj;
     }
     public FiksniBroj(Grad grad, String broj) {
         this.grad=grad;
         this.broj=broj;
+    }
+    @Override
+    public int hashCode() {
+        return grad.hashCode();
+    }
+    @Override
+    public boolean equals (Object o) {
+        if (o == null) {
+            return false;
+        }
+        else if (o instanceof FiksniBroj) {
+            FiksniBroj fiksni = (FiksniBroj) o;
+            return fiksni.broj.equals(this.broj) && fiksni.hashCode() == this.grad.hashCode();
+        }
+        else return false;
     }
 }
