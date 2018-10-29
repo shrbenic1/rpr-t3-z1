ffd9f9cf0abe986fa6b6d85364085f2f9c4495c5
shrbenic1 <shrbenic1@etf.unsa.ba>
2018-10-30 00:00:39 +0100

    Dodane metode naSlovo izGrada i izGradaBrojevi (samo okvirno)

--- src/ba/unsa/etf/rpr/tutorijal03/Imenik.java
+++ src/ba/unsa/etf/rpr/tutorijal03/Imenik.java
@@ -1,8 +1,12 @@
 package ba.unsa.etf.rpr.tutorijal03;
 
+import static ba.unsa.etf.rpr.tutorijal03.FiksniBroj.Grad;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.Set;
 
 public class Imenik {
+
     private HashMap <String, TelefonskiBroj> imenikPoImenu = new HashMap<>();
     private HashMap <TelefonskiBroj, String> imenikPoBroju = new HashMap<>();
 
@@ -16,10 +20,15 @@ public class Imenik {
         }
         else return null;
     }
+
     public String dajIme(TelefonskiBroj broj) {
         if(imenikPoBroju.containsKey(broj)) {
             return (String) imenikPoBroju.get(broj);
         }
         else return null;
     }
+
+    String naSlovo(char s) {return "";}
+    Set<String> izGrada(Grad g) {return Collections.emptySet();}
+    Set<TelefonskiBroj>izGradaBrojevi(Grad g) {return Collections.emptySet();}
 }
