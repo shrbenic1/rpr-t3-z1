b8df10e93ea8ef1ae0229c8cc16f97ad26e3c0db
origin <shrbenic1@etf.unsa.ba>
2018-10-30 17:39:12 +0100

    Promijenjena metoda dajIme i postavljena samo jedna HashMapa kao atribut klase Imenik

--- src/ba/unsa/etf/rpr/tutorijal03/Imenik.java
+++ src/ba/unsa/etf/rpr/tutorijal03/Imenik.java
@@ -2,33 +2,38 @@ package ba.unsa.etf.rpr.tutorijal03;
 
 import static ba.unsa.etf.rpr.tutorijal03.FiksniBroj.Grad;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 
 public class Imenik {
 
-    private HashMap <String, TelefonskiBroj> imenikPoImenu = new HashMap<>();
-    private HashMap <TelefonskiBroj, String> imenikPoBroju = new HashMap<>();
+    private HashMap <String, TelefonskiBroj> imenik = new HashMap<>();
 
     public void dodaj(String ime, TelefonskiBroj broj) {
-        imenikPoBroju.put(broj, ime);
-        imenikPoImenu.put(ime, broj);
+        imenik.put(ime, broj);
     }
     public String dajBroj(String ime) {
-        if(imenikPoImenu.containsKey(ime)) {
-            return ((TelefonskiBroj) imenikPoImenu.get(ime)).ispisi();
+        if(imenik.containsKey(ime)) {
+            return ((TelefonskiBroj) imenik.get(ime)).ispisi();
         }
         else return null;
     }
 
     public String dajIme(TelefonskiBroj broj) {
-        if(imenikPoBroju.containsKey(broj)) {
-            return (String) imenikPoBroju.get(broj);
+        for(Map.Entry<String, TelefonskiBroj> entry : imenik.entrySet()) {
+            String key = entry.getKey();
+            TelefonskiBroj brojTelefona = entry.getValue();
+            if(brojTelefona.equals(broj)) return key;
         }
-        else return null;
+        return null;
     }
 
-    String naSlovo(char s) {return "";}
+    String naSlovo(char s) {
+
+        return "";
+    }
     Set<String> izGrada(Grad g) {return Collections.emptySet();}
     Set<TelefonskiBroj>izGradaBrojevi(Grad g) {return Collections.emptySet();}
 }
